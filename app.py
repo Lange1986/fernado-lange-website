5c40502d85d9ada3d671c4f525915ea4dfa9b3a9
Fernando Lange <149966950+Lange1986@users.noreply.github.com>
2024-12-05 15:55:23 +0000

    Adicionar dados dinamicos API route

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
+CONTACTOS = [
+
+    { 'id': 1, 
+     'nome': 'Lange', 
+     'celular': '865890288',
+     'email': 'lange86@gmail.com',
+     'profissão': 'Engenheiro de Software'
+    },
+
+    { 'id': 2,
+     'nome': 'Pedro',
+     'celular': '845890288',
+     'email': 'pedro86@gmail.com',
+     'profissão': 'Engenheiro Backend'
+    },
+
+    { 'id': 3,
+     'nome': 'Kendrick',
+     'celular': '875890288',
+     'email': 'ken86@gmail.com',
+     'profissão': 'Engenheiro de Frontend'
+     
+    }
+]
+
+
 @app.route("/")
 def hello():
-    return render_template("home.html")
+    return render_template("home.html", contactos=CONTACTOS)
+
+
+@app.route("/api/contactos")
+def lista_contactos():
+    return jsonify(CONTACTOS)
 
 
 
